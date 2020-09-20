dac0a9b98d38434a18dfb4aa86a700434c7de98a
Balaji Shanmugam <balajiraaj@outlook.com>
2020-09-20 21:14:01 +0530

    Create docker container with image

--- QuoteManagement/Models/QuoteDetails.cs
+++ QuoteManagement/Models/QuoteDetails.cs
@@ -1,6 +1,7 @@
 ﻿using QuoteManagement.Validation;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QuoteManagement.Models
 {
@@ -22,13 +23,13 @@ namespace QuoteManagement.Models
         public string EmailID { get; set; }
         [Display(Name = "Mobile Number"), RegularExpression("([0-9]+)", ErrorMessage = "Please enter valid number"), MaxLength(10), MinLength(10, ErrorMessage = "Please enter 10 digits Mobile number"), Required(ErrorMessage = "Please enter Mobile Number")]
         public string MobileNumber { get; set; }
-        [Display(Name = "Investment Amount")]
+        [Display(Name = "Investment Amount"), Column(TypeName = "decimal(18,2)")]
         public decimal InvestmentAmount { get; set; }
         [Display(Name = "Retirement Age"), Range(60, 75, ErrorMessage = "Retirement age must between 60 to 75"), Required(ErrorMessage = "Please enter Retirement Age")]
         public int RetirementAge { get; set; }
         [Display(Name = "Pension Plan"), EnumDataType(typeof(PensionPlans)), Required(ErrorMessage = "Please enter Pension Plan"), Range(1, 3, ErrorMessage = "Please enter correct pension value. /n 1. Pension Silver, /n 2. Pension Gold, /n 3. Pension Platinum.")]
         public PensionPlans PensionPlan { get; set; }
-        [Display(Name = "Maturity Amount")]
+        [Display(Name = "Maturity Amount"), Column(TypeName = "decimal(18,2)")]
         public decimal MaturityAmount { get; set; }
     }
 
